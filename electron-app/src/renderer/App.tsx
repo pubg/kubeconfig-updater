5cbb6e9642de6651001fd4274d72d0cc88909e73
do.kim <do.kim@pubg.com>
2021-12-12 18:04:27 +0900

    add sample main page

--- electron-app/src/renderer/App.tsx
+++ electron-app/src/renderer/App.tsx
@@ -2,25 +2,22 @@ import { Paper } from '@mui/material'
 import { MemoryRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
 import Sidebar from './components/sidebar/sidebar'
-
-const Hello = () => {
-  return (
-    <Paper sx={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
-      <Paper
-        elevation={3}
-        sx={{ display: 'flex', width: '280px', height: '100%' }}
-      >
-        <Sidebar />
-      </Paper>
-    </Paper>
-  )
-}
+import Home from './pages/home/home'
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Hello} />
+        <Paper sx={{ height: '100vh', display: 'flex', flexDirection: 'row' }}>
+          <Paper
+            elevation={3}
+            sx={{ display: 'flex', width: '280px', height: '100%' }}
+          >
+            <Sidebar />
+          </Paper>
+          <Route path="/" component={Home} />
+          <Route path="/cluster-management" component={} />
+        </Paper>
       </Switch>
     </Router>
   )
---
+++ electron-app/src/renderer/pages/home/home.tsx
@@ -0,0 +1,11 @@
+import { Container, Typography } from '@mui/material'
+
+export default function Home() {
+  return (
+    <Container
+      sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+    >
+      <p>Main Page</p>
+    </Container>
+  )
+}
