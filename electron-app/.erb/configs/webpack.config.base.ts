d8e84b08bccc6f1034d6f4e62e686e015b22598c
do.kim <do.kim@pubg.com>
2021-12-12 19:52:11 +0900

    add crypto-browserify, fluentUI

--- electron-app/.erb/configs/webpack.config.base.ts
+++ electron-app/.erb/configs/webpack.config.base.ts
@@ -41,6 +41,9 @@ export default {
   resolve: {
     extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
     modules: [webpackPaths.srcPath, 'node_modules'],
+    fallback: {
+      crypto: require.resolve('crypto-browserify')
+    }
   },
 
   plugins: [
