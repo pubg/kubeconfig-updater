4d0178fcda78d30df2189c6eaa9a91658b53cc0f
do.kim <do.kim@pubg.com>
2021-12-13 15:10:18 +0900

    implement detailsList

--- electron-app/src/renderer/pages/clusterManagement/clusterManagement.tsx
+++ electron-app/src/renderer/pages/clusterManagement/clusterManagement.tsx
@@ -1,19 +1,24 @@
-import { DetailsList, DetailsListLayoutMode } from '@fluentui/react'
-import { Refresh } from '@mui/icons-material'
+import { DetailsList, IColumn } from '@fluentui/react'
+import { MoreVertOutlined, Refresh } from '@mui/icons-material'
 import {
   Autocomplete,
   Box,
   Button,
   Checkbox,
-  Container as Box,
   FormControlLabel,
   FormGroup,
+  IconButton,
   Paper,
   Stack,
   Switch,
   TextField,
+  Typography,
 } from '@mui/material'
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
+import {
+  ClusterInfo,
+  Status,
+} from '../../../shared/models/clusterInfo/clusterInfo'
 import { generateMockClusterInfos } from '../../../shared/models/clusterInfo/mockClusterInfo'
 
 const mockTags = ['stage', 'vendor', 'region']
@@ -23,16 +28,115 @@ const items = generateMockClusterInfos(64)
 export default function ClusterManagement() {
   const [showRegistered, setShowRegistered] = useState(false)
 
+  const colorFormatForStatus = (status: Status) => {
+    switch (status) {
+      case 'Registered':
+        return 'success'
+
+      case 'Unregistered':
+        return 'info'
+
+      case 'Unknown':
+        return 'warning'
+
+      case 'Unauthorized':
+        return 'error'
+
+      default:
+        throw new Error()
+    }
+  }
+
+  const itemFilter = (item: ClusterInfo) => {
+    return showRegistered || (!showRegistered && item.status !== 'Registered')
+  }
+
+  const columns: IColumn[] = [
+    {
+      key: 'clusterName',
+      name: 'Cluster Name',
+      fieldName: 'clusterName',
+      minWidth: 0,
+      isResizable: true,
+    },
+    {
+      key: 'vendor',
+      name: 'Infra Vendor',
+      fieldName: 'vendor',
+      minWidth: 0,
+      isResizable: true,
+    },
+    {
+      key: 'account',
+      name: 'Account',
+      fieldName: 'account',
+      minWidth: 256,
+      isResizable: true,
+    },
+    {
+      key: 'status',
+      name: 'Status',
+      minWidth: 0,
+      isResizable: true,
+      onRender: (item: ClusterInfo) => {
+        return (
+          <Typography color={colorFormatForStatus(item.status)}>
+            {item.status}
+          </Typography>
+        )
+      },
+    },
+    {
+      key: 'action',
+      name: 'action',
+      minWidth: 64,
+      isResizable: true,
+      onRender: (item: ClusterInfo) => {
+        return (
+          <IconButton color="primary">
+            <MoreVertOutlined />
+          </IconButton>
+        )
+      },
+    },
+  ]
+
+  const renderItemColumn = useCallback(
+    (item?: any, index?: number, column?: IColumn) => {
+      return <div />
+    },
+    []
+  )
+
   return (
     /** background */
     <Paper sx={{ width: '100%', height: '100%' }}>
       {/* actual container */}
-      <Box sx={{ width: '100%', height: '100%', marginTop: '32px' }}>
+      <Box
+        sx={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'stretch',
+        }}
+      >
         {/* Header Menu Container */}
-        <Box sx={{ display: 'flex', margin: '32px' }}>
-          {/*  */}
-          <Box />
-          <Stack direction="row" width="100%" justifyContent="space-between">
+        <Paper
+          elevation={0}
+          sx={{
+            width: '100%',
+            height: '128px',
+            display: 'flex',
+            borderBottom: '2px solid gray',
+          }}
+        >
+          <Stack
+            direction="row"
+            width="100%"
+            justifyContent="space-between"
+            margin="32px 32px 32px 32px"
+          >
             <FormGroup row sx={{ gap: '16px', alignItems: 'center' }}>
               <TextField
                 size="small"
@@ -53,7 +157,7 @@ export default function ClusterManagement() {
                 options={mockTags}
                 disableCloseOnSelect
                 getOptionLabel={(opt) => opt}
-                style={{ width: '512px' }}
+                style={{ width: '256px' }}
                 size="small"
                 renderInput={(params) => (
                   // eslint-disable-next-line react/jsx-props-no-spreading
@@ -74,16 +178,38 @@ export default function ClusterManagement() {
               </Button>
             </Stack>
           </Stack>
-        </Box>
+        </Paper>
 
         {/* list container */}
-        <Box overflow="scroll" width="100%" height="100%">
+        <Box
+          sx={{
+            height: '100%',
+            overflow: 'hidden',
+            overflowY: 'scroll',
+            marginTop: '4px',
+          }}
+        >
           <DetailsList
-            items={items.filter(
-              (item) => !item.registered || (showRegistered && item.registered)
-            )}
-            layoutMode={DetailsListLayoutMode.justified}
+            items={items.filter(itemFilter)}
+            columns={columns}
+            // layoutMode={DetailsListLayoutMode.justified}
+            // onRenderItemColumn={renderItemColumn}
           />
+        </Box>
+
+        {/* bottom sidebar container */}
+        <Box
+          width="100%"
+          height="64px"
+          display="flex"
+          alignItems="center"
+          margin="8px"
+          paddingLeft="16px"
+        >
+          <Stack direction="row" width="100%" alignItems="center" gap="16px">
+            <Button variant="outlined">Register ALL</Button>
+            <Typography>0 Clusters Selected.</Typography>
+          </Stack>
         </Box>
       </Box>
     </Paper>
