8497ad6bb063defe21bb347ec35739c9d95b7188
do.kim <do.kim@pubg.com>
2021-12-17 19:19:43 +0900

    fixf hack/gen_protoc_js.sh

--- hack/gen_protoc_js.sh
+++ hack/gen_protoc_js.sh
@@ -6,10 +6,10 @@
 
 # set -eux
 
-SRC_PATH="./backend/protos"
+SRC_PATH="./core/protos"
 OUTPUT_PATH="./electron-app/src/renderer"
 
 rm -rf "$OUTPUT_PATH/protos"
 mkdir "$OUTPUT_PATH/protos"
 
-protoc "$SRC_PATH/common.proto" "$SRC_PATH/kubeconfig_service.proto" -I="./backend"  --grpc_web_out="import_style=typescript,mode=grpcwebtext:$OUTPUT_PATH" --js_out="import_style=commonjs:$OUTPUT_PATH"
+protoc "$SRC_PATH/common.proto" "$SRC_PATH/kubeconfig_service.proto" -I="./core"  --grpc_web_out="import_style=typescript,mode=grpcwebtext:$OUTPUT_PATH" --js_out="import_style=commonjs:$OUTPUT_PATH"
