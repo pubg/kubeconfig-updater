7cfb8b3f6e46e1c77f92b80b3c241b1d78c07f7f
do.kim <do.kim@pubg.com>
2021-12-20 17:01:53 +0900

    refactor clusterMetadataRequester

--- electron-app/src/renderer/components/clusterMetadataRequester.ts
+++ electron-app/src/renderer/components/clusterMetadataRequester.ts
@@ -1,48 +1,97 @@
 /* eslint-disable func-names */
 import React from 'react'
-import { action, computed, flow, makeObservable, observable } from 'mobx'
-import { container, singleton } from 'tsyringe'
+import { flow, makeObservable, observable } from 'mobx'
+import { container } from 'tsyringe'
+import dayjs, { Dayjs } from 'dayjs'
 import { AggregatedClusterMetadata } from '../protos/kubeconfig_service_pb'
 import GetAvailableClusterService from '../services/getAvailableClusters'
+import SyncAvailableClustersService from '../services/syncAvailableClusters'
 
-// REFACTOR: this store is trying to be "god class" should break down this
-@singleton()
+/**
+ * manages cluster metadata requesting
+ */
 export class ClusterMetadataRequester {
   constructor() {
     makeObservable(this)
   }
 
   @observable
-  state: 'ready' | 'fetching' = 'ready'
+  state: 'ready' | 'fetch' | 'in-sync' = 'ready'
 
   @observable
   items: AggregatedClusterMetadata[] = []
 
-  fetchMetadata = flow(function* (this: ClusterMetadataRequester) {
-    this.state = 'fetching'
-    this.items = []
+  @observable
+  // minute
+  ResyncInterval = 5
 
-    const fetch = async () => {
-      const req = container.resolve(GetAvailableClusterService)
-      const res = await req.request()
+  @observable
+  private _lastSynced: Dayjs | null = null
 
-      return res.getClustersList()
+  get lastSynced(): Dayjs | null {
+    return this._lastSynced
+  }
+
+  set lastSynced(time: Dayjs | null) {
+    this._lastSynced = time
+  }
+
+  get shouldResync(): boolean {
+    if (this.lastSynced === null) {
+      return true
     }
 
+    const now = dayjs()
+    if (now.diff(this.lastSynced, 'minute') >= this.ResyncInterval) {
+      return true
+    }
+
+    return false
+  }
+
+  fetchMetadata = flow(function* (this: ClusterMetadataRequester) {
+    this.items = []
+
+    if (this.shouldResync) {
+      this.state = 'in-sync'
+
+      try {
+        yield ClusterMetadataRequester.sync()
+      } catch (e) {
+        console.error(e)
+      }
+    }
+
+    this.state = 'fetch'
+
     try {
-      this.items = yield fetch()
-      const clusterList: AggregatedClusterMetadata[] = yield fetch()
+      this.items = yield ClusterMetadataRequester.fetch()
     } catch (e) {
+      // TODO: replace this to winston logger
       console.error(e)
     }
 
     this.state = 'ready'
   })
+
+  private static async sync() {
+    const req = container.resolve(SyncAvailableClustersService)
+    // TODO: does this throw error or do I have to use error handling with returned value?
+    // does it even need try/catch? need to read doc first...
+    return req.request()
+  }
+
+  private static async fetch() {
+    const req = container.resolve(GetAvailableClusterService)
+    const res = await req.request()
+
+    return res.getClustersList()
+  }
 }
 
 export const ClusterMetadataRequesterContext = React.createContext<ClusterMetadataRequester | null>(null)
 
-export const useStore = (): ClusterMetadataRequester => {
+export const useContext = (): ClusterMetadataRequester => {
   const store = React.useContext(ClusterMetadataRequesterContext)
   if (!store) {
     throw new Error(`tried to use ${ClusterMetadataRequester.name} but object is null.`)
