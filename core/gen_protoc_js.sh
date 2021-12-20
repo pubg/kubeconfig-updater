a3f07b65ca8f3f5c48f9aea47c0aa550c55d721a
Minuk Song <musong@pubg.com>
2021-12-20 14:21:55 +0900

    Update frontend codes

--- core/gen_protoc_js.sh
+++ core/gen_protoc_js.sh
@@ -12,4 +12,4 @@ OUTPUT_PATH="./../electron-app/src/renderer"
 rm -rf "$OUTPUT_PATH/protos"
 mkdir "$OUTPUT_PATH/protos"
 
-protoc "$SRC_PATH/common.proto" "$SRC_PATH/kubeconfig_service.proto" -I="./core"  --grpc_web_out="import_style=typescript,mode=grpcweb:$OUTPUT_PATH" --js_out="import_style=commonjs:$OUTPUT_PATH"
+protoc "$SRC_PATH/common.proto" "$SRC_PATH/kubeconfig_service.proto" -I="./"  --grpc-web_out="import_style=typescript,mode=grpcweb:$OUTPUT_PATH" --js_out="import_style=commonjs:$OUTPUT_PATH"
