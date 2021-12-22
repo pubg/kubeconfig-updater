918641c54d492e04f2b46ca11fb5878b4df87f17
do.kim <90447762+do-kim-pubg@users.noreply.github.com>
2021-12-22 17:18:27 +0900

    hotfix build issue (#17)
    * replace npm to pnpm
    
    * prevent using pino-pretty on production
    
    * fix crashing when exiting app
    
    replace pino.destination to NOP writer
    
    * remove unwanted env values on build

--- electron-app/src/logger/mainLogger.ts
+++ electron-app/src/logger/mainLogger.ts
@@ -1,18 +1,27 @@
 import pino from 'pino'
 
-const mainLogger = pino({
+const devOptions: pino.LoggerOptions = {
   name: 'main',
-  // TODO: make this configurable based on dev/prod
   level: 'debug',
   transport: {
     target: 'pino-pretty',
-    // https://github.com/pinojs/pino-pretty#options
     options: {
       colorize: true,
       timestamp: false,
       ignore: 'pid,hostname',
     },
   },
-})
+}
+
+// TODO: add pino-pretty
+const prodOptions: pino.LoggerOptions = {
+  name: 'main',
+  level: 'error',
+}
+
+const isDevelopment = process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true'
+
+// TODO: replace hotfix destination to electron-managed logger
+const mainLogger = pino(isDevelopment ? devOptions : prodOptions, { write: () => {} })
 
 export default mainLogger
