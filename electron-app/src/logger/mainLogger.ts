05ec68a8dfc398ff1732349151bc6383e6a4810e
do.kim <do.kim@pubg.com>
2021-12-22 16:43:33 +0900

    prevent using pino-pretty on production

--- electron-app/src/logger/mainLogger.ts
+++ electron-app/src/logger/mainLogger.ts
@@ -1,18 +1,26 @@
 import pino from 'pino'
+import 'pino-pretty'
 
-const mainLogger = pino({
+const devOptions: pino.LoggerOptions = {
   name: 'main',
-  // TODO: make this configurable based on dev/prod
   level: 'debug',
   transport: {
     target: 'pino-pretty',
-    // https://github.com/pinojs/pino-pretty#options
     options: {
       colorize: true,
       timestamp: false,
       ignore: 'pid,hostname',
     },
   },
-})
+}
+
+const prodOptions: pino.LoggerOptions = {
+  name: 'main',
+  level: 'error',
+}
+
+const isDevelopment = process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true'
+
+const mainLogger = pino(isDevelopment ? devOptions : prodOptions)
 
 export default mainLogger
