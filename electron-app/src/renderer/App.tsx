797d8f81cf25cbb6fa6212110c7be8d89f8c82b7
do.kim <do.kim@pubg.com>
2021-12-22 17:39:02 +0900

    add register progress popup, move app context to root

--- electron-app/src/renderer/App.tsx
+++ electron-app/src/renderer/App.tsx
@@ -1,49 +1,58 @@
 import { Paper } from '@mui/material'
 import { MemoryRouter as Router, Switch, Route } from 'react-router-dom'
-import { container } from 'tsyringe'
 import './App.css'
 import Sidebar from './components/sidebar'
-import MockKubeconfigClient from './mock/grpc/grpcClient'
 import ClusterManagement from './pages/clusterManagement/page'
 import Home from './pages/home/home'
-import { KubeconfigClient } from './protos/Kubeconfig_serviceServiceClientPb'
 import About from './pages/about/about'
-
-// TODO: delete this, only for testing.
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-// container.register(KubeconfigClient, { useClass: MockKubeconfigClient as any })
+import RegisterProgressSnackbar from './components/registerProgressPopup'
+import * as containerHooks from './hooks/container'
+import { ClusterMetadataRequester, ClusterMetadataRequesterContext } from './components/clusterMetadataRequester'
+import { ClusterRegisterRequester, ClusterRegisterRequesterContext } from './components/clusterRegisterRequester'
+import { ClusterMetadataStore, ClusterMetadataStoreContext } from './pages/clusterManagement/clusterMetadataStore'
 
 export default function App() {
+  const clusterMetadataStore = containerHooks.useResolve(ClusterMetadataStore)
+  const clusterRegisterRequester = containerHooks.useResolve(ClusterRegisterRequester)
+  const clusterMetadataRequester = containerHooks.useResolve(ClusterMetadataRequester)
+
   return (
-    <Router>
-      <Paper
-        sx={{
-          width: '100vw',
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'stretch',
-          alignItems: 'stretch',
-        }}
-      >
-        <Paper
-          elevation={3}
-          sx={{
-            display: 'flex',
-            width: '280px',
-            height: '100%',
-            flexShrink: 0,
-            flexGrow: 0,
-          }}
-        >
-          <Sidebar />
-        </Paper>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/cluster-management" component={ClusterManagement} />
-          <Route path="/about" component={About} />
-        </Switch>
-      </Paper>
-    </Router>
+    <ClusterMetadataRequesterContext.Provider value={clusterMetadataRequester}>
+      <ClusterRegisterRequesterContext.Provider value={clusterRegisterRequester}>
+        <ClusterMetadataStoreContext.Provider value={clusterMetadataStore}>
+          <Router>
+            <Paper
+              sx={{
+                width: '100vw',
+                height: '100vh',
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'stretch',
+                alignItems: 'stretch',
+              }}
+            >
+              <Paper
+                elevation={3}
+                sx={{
+                  display: 'flex',
+                  width: '280px',
+                  height: '100%',
+                  flexShrink: 0,
+                  flexGrow: 0,
+                }}
+              >
+                <Sidebar />
+              </Paper>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/cluster-management" component={ClusterManagement} />
+                <Route path="/about" component={About} />
+              </Switch>
+              <RegisterProgressSnackbar />
+            </Paper>
+          </Router>
+        </ClusterMetadataStoreContext.Provider>
+      </ClusterRegisterRequesterContext.Provider>
+    </ClusterMetadataRequesterContext.Provider>
   )
 }
--- electron-app/src/renderer/components/registerProgressPopup.tsx
+++ electron-app/src/renderer/components/registerProgressPopup.tsx
@@ -1,6 +1,17 @@
-import { Snackbar } from '@mui/material'
+import { Snackbar, Typography } from '@mui/material'
 import { observer } from 'mobx-react-lite'
+import { useContext } from './clusterRegisterRequester'
 
 export default observer(function RegisterProgressPopup() {
-  return <Snackbar />
+  const requester = useContext()
+
+  const action = <></>
+
+  return (
+    <Snackbar open={requester.state === 'processing'}>
+      <Typography>
+        processing {requester.processedCount} / {requester.length}
+      </Typography>
+    </Snackbar>
+  )
 })
