4b566f8cdef0dcabf2a42fe03bc1a688e8da5e2f
do.kim <do.kim@pubg.com>
2021-12-22 19:59:01 +0900

    WIP: implementing register progress popup

--- electron-app/src/renderer/components/registerProgressPopup.tsx
+++ electron-app/src/renderer/components/registerProgressPopup.tsx
@@ -1,17 +1,60 @@
-import { Snackbar, Typography } from '@mui/material'
+import { ProgressIndicator } from '@fluentui/react'
+import { Paper, Snackbar, SnackbarCloseReason, SnackbarContent, SnackbarProps, Typography } from '@mui/material'
+import { autorun, reaction, when } from 'mobx'
 import { observer } from 'mobx-react-lite'
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import logger from '../../logger/logger'
+import { useAutorun } from '../hooks/mobx'
 import { useContext } from './clusterRegisterRequester'
 
 export default observer(function RegisterProgressPopup() {
   const requester = useContext()
 
-  const action = <></>
+  const [open, setOpen] = useState(false)
+  const [autoHideDuration, setAutoHideDuration] = useState<number | null>(null)
+  const desiredAutoHideDuration = 3000 // 3 second
+
+  const description = useMemo(() => {
+    const msg = `[${requester.processedCount}/${requester.length}] adding cluster: ${requester.currentItem?.clusterName}`
+
+    return msg
+  }, [requester.currentItem?.clusterName, requester.length, requester.processedCount])
+
+  const percentageComplete = useMemo<number>(() => {
+    return requester.processedCount / (requester.length || 1)
+  }, [requester.length, requester.processedCount])
+
+  useAutorun(() => {
+    if (requester.state === 'processing') {
+      setAutoHideDuration(null)
+      setOpen(true)
+    } else {
+      setAutoHideDuration(desiredAutoHideDuration)
+    }
+  })
+
+  const onClose = useCallback<NonNullable<SnackbarProps['onClose']>>((e, reason) => {
+    if (reason === 'clickaway') {
+      setOpen(false)
+      setAutoHideDuration(null)
+    }
+  }, [])
 
   return (
-    <Snackbar open={requester.state === 'processing'}>
-      <Typography>
-        processing {requester.processedCount} / {requester.length}
-      </Typography>
+    <Snackbar
+      open={open}
+      // open
+      autoHideDuration={autoHideDuration}
+      anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+      onClose={onClose}
+    >
+      <Paper elevation={4}>
+        <ProgressIndicator
+          label="Register Clusters..."
+          description={description}
+          percentComplete={percentageComplete}
+        />
+      </Paper>
     </Snackbar>
   )
 })
