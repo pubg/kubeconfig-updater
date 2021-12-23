c1261dfbaa0070519a7875a7088fb8a996726ec6
do.kim <do.kim@pubg.com>
2021-12-23 17:55:15 +0900

    feature: strict typing for ipc connection in preload.js

---
+++ electron-app/src/types/shared.d.ts
@@ -0,0 +1,18 @@
+import 'electron'
+
+declare global {
+  namespace IPCType {
+    type IPCPortRequestType = 'getGrpcWebPort'
+    type IPCThemeType = 'theme' | 'theme:getTheme'
+  }
+
+  type IPCType = IPCType.IPCPortRequestType | IPCType.IPCThemeType | 'ipc-test'
+
+  namespace Electron {
+    interface ContextBridge {
+      exposeInMainWorld(apiKey: IPCType, api: any): void
+    }
+  }
+}
+
+export {}
