a6a7845b93b138877aac2b9514399eb1df7eb597
do.kim <do.kim@pubg.com>
2021-12-27 20:22:30 +0900

    change exec to execFile, trim unnecessary log info

--- electron-app/src/main/backend/backend.ts
+++ electron-app/src/main/backend/backend.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import _ from 'lodash'
-import { exec, ChildProcess } from 'child_process'
-import { inject, injectable, singleton } from 'tsyringe'
+import { ChildProcess, execFile } from 'child_process'
+import { inject, singleton } from 'tsyringe'
 import { dialog } from 'electron'
 import kill from 'tree-kill'
 import { BackendExecCmd, BackendExecCwd, BackendGrpcPort, BackendGrpcWebPort } from './symbols'
@@ -12,13 +12,15 @@ import mainLogger from '../logger/mainLogger'
  */
 @singleton()
 export default class BackendManager {
+  private readonly backendLogger = mainLogger.scope('BackendManager')
+
   private process: ChildProcess | null = null
 
   private _status: 'running' | 'on-exit' | 'exited' = 'running'
 
   private readonly _grpcPort: number
 
-  private readonly _grpbWebPort: number
+  private readonly _grpcWebPort: number
 
   private errorCount: number = 0
 
@@ -31,7 +33,7 @@ export default class BackendManager {
   }
 
   get grpbWebPort(): number {
-    return this._grpbWebPort
+    return this._grpcWebPort
   }
 
   constructor(
@@ -41,24 +43,29 @@ export default class BackendManager {
     @inject(BackendGrpcWebPort) grpcWebPort: number
   ) {
     this._grpcPort = grpcPort || _.random(10000, 20000, false)
-    this._grpbWebPort = grpcWebPort || _.random(10000, 20000, false)
+    this._grpcWebPort = grpcWebPort || _.random(10000, 20000, false)
   }
 
   start() {
-    const cmd = `${this.cmd} server --port=${this._grpcPort} --web-port=${this._grpbWebPort}`
-    mainLogger.info(`[BackendManager] CMD ${cmd}`)
-    mainLogger.info(`[BackendManager] CWD ${this.cwd}`)
-    this.process = exec(cmd, { cwd: this.cwd }, (err, stdout, stderr) => {
-      // if backend tree-killed, it returns exit code 1 which is expected and normal.
-      if (err && this.status !== 'on-exit') {
-        mainLogger.error(err)
+    const cmd = `${this.cmd} server --port=${this._grpcPort} --web-port=${this._grpcWebPort}`
+    this.backendLogger.info(`CMD ${cmd}`)
+    this.backendLogger.info(`CWD ${this.cwd}`)
+    this.process = execFile(
+      this.cmd,
+      ['server', `--port=${this._grpcPort}`, `--web-port=${this._grpcWebPort}`],
+      { cwd: this.cwd },
+      (err) => {
+        // if backend tree-killed, it returns exit code 1 which is expected and normal.
+        if (err && this.status !== 'on-exit') {
+          this.backendLogger.error(err)
+        }
       }
-    })
+    )
 
     this.process.stdout?.on('data', (data: string) => {
       data.split('\n').forEach((line) => {
         if (line.trim() !== '') {
-          mainLogger.info(`[BackendManager] StdOut: ${line}`)
+          this.backendLogger.info(`StdOut: ${line}`)
         }
       })
     })
@@ -66,46 +73,46 @@ export default class BackendManager {
     this.process.stderr?.on('data', (data: string) => {
       data.split('\n').forEach((line) => {
         if (line.trim() !== '') {
-          mainLogger.error(`[BackendManager] StdErr: ${line}`)
+          this.backendLogger.error(`StdErr: ${line}`)
         }
       })
     })
 
     this.process.on('error', (e) => {})
     this.process.on('exit', async (e) => {
-      mainLogger.info(`[BackendManager] recerived childprocess exit event, status:${this._status}`)
+      this.backendLogger.info(`recerived childprocess exit event, status:${this._status}`)
       if (this._status === 'running') {
         this.errorCount += 1
         if (this.errorCount >= 10) {
-          mainLogger.error(
+          this.backendLogger.error(
             await dialog.showMessageBox({
               message: 'Cannot Connect to Logic Controller Process',
             })
           )
           process.exit(1)
         }
-        mainLogger.warn('[BackendManager] kubeconfig-updater-backend died, try restart')
+        this.backendLogger.warn('kubeconfig-updater-backend died, try restart')
         this.start()
       }
     })
   }
 
   async end() {
-    mainLogger.info('[BackendManager] change backendmanager status to exited')
+    this.backendLogger.info('change backendmanager status to exited')
     this._status = 'on-exit'
 
     const currProcess = this.process
 
     if (currProcess) {
-      mainLogger.info(`[BackendManager] tree kill backend process pid:${currProcess.pid}`)
+      this.backendLogger.info(`tree kill backend process pid:${currProcess.pid}`)
 
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       const err = await new Promise((resolve) => kill(currProcess.pid!, resolve))
 
       if (err) {
-        mainLogger.error(`[BackendManager] tree kill process occurred error ${err}`)
+        this.backendLogger.error(`tree kill process occurred error ${err}`)
       }
-      mainLogger.info('[BackendManager] kill process finished')
+      this.backendLogger.info('kill process finished')
       this.process = null
     }
 
