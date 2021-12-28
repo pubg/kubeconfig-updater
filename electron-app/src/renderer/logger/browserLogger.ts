e461f8f90f806854753b7e684c66a04e650d697b
do.kim <90447762+do-kim-pubg@users.noreply.github.com>
2021-12-28 15:47:23 +0900

    handle window.electronLogger not defined on web browser (#38)

--- electron-app/src/renderer/logger/browserLogger.ts
+++ electron-app/src/renderer/logger/browserLogger.ts
@@ -1,7 +1,10 @@
 declare namespace window {
-  const electronLogger: typeof import('electron-log')['functions']
+  const electronLogger: typeof import('electron-log')['functions'] | undefined
 }
 
-const { electronLogger: browserLogger } = window
+const { electronLogger } = window
+
+// in the dev-mode browser, preload.js not kicked in so electronLogger is undefined
+const browserLogger = electronLogger ?? console
 
 export default browserLogger
