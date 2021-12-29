510e5843f9a63efefcf982a5080c3267339c5b64
Minuk Song <musong@pubg.com>
2021-12-29 19:53:32 +0900

    Add macos universal architecture type when build

--- build/set-version.sh
+++ build/set-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION=$1
-ROOT_PWD="$PWD"
+BASE_DIR=$(dirname "$0")
 
 if [ -z "$VERSION" ]; then
   echo "version is not defined. please pass valid semver"
@@ -11,9 +11,9 @@ fi
 echo "CURRENT VERSION: $VERSION"
 
 # change electron-app package.json version
-OUTPUT=$(cd "$ROOT_PWD/electron-app" && pnpm version "$VERSION")
+OUTPUT=$(cd "$BASE_DIR/../electron-app" && pnpm version "$VERSION")
 echo "electron-app version: $OUTPUT"
 
 ## change electron-app/release
-OUTPUT=$(cd "$ROOT_PWD/electron-app/release/app" && pnpm version "$VERSION")
+OUTPUT=$(cd "$BASE_DIR/../electron-app/release/app" && pnpm version "$VERSION")
 echo "electron-app/release version: $OUTPUT"
