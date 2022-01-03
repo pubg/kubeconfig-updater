1c58066d8d718279a2861475af0ea190999dff50
do.kim <do.kim@pubg.com>
2022-01-03 15:34:51 +0900

    useResolve: use useMemo instead of useState

--- electron-app/src/renderer/hooks/container.ts
+++ electron-app/src/renderer/hooks/container.ts
@@ -1,11 +1,11 @@
 /* eslint-disable import/prefer-default-export */
-import { useState } from 'react'
+import { useMemo } from 'react'
 import { DependencyContainer, InjectionToken, container as defaultContainer } from 'tsyringe'
 
 export function useResolve<T>(token: InjectionToken<T>, c?: DependencyContainer): T {
   const container = c ?? defaultContainer
 
-  const [instance] = useState(() => container.resolve(token))
+  const instance = useMemo(() => container.resolve(token), [container, token])
 
   return instance
 }
