57dd027c0aaf0e92a49b9eb50fe44fdfc16e843a
do.kim <do.kim@pubg.com>
2022-01-11 12:28:03 +0900

    refactor clusterRegisterStore to use payloadStore

--- electron-app/src/renderer/store/clusterRegisterStore.ts
+++ electron-app/src/renderer/store/clusterRegisterStore.ts
@@ -1,13 +1,27 @@
-import { flow, makeObservable, observable } from 'mobx'
+import { computed, flow, makeAutoObservable, makeObservable, observable } from 'mobx'
 import { container, singleton } from 'tsyringe'
+import { OBSERVED } from '../../types/mobx'
 import EventStore from '../event/eventStore'
 import browserLogger from '../logger/browserLogger'
 import { CommonRes, ResultCode } from '../protos/common_pb'
 import ClusterRepository from '../repositories/clusterRepository'
+import { PayloadMap, ValueWithPayload } from '../types/payloadMap'
 
 type Item = {
   clusterName: string
   accountId: string
+}
+
+type Payload = OBSERVED<{
+  resolved?: boolean
+  response?: {
+    resultCode: ResultCode
+    message: string
+  }
+}>
+
+function keySelector(item: Item): string {
+  return `${item.accountId}#${item.clusterName}`
 }
 
 @singleton()
@@ -17,66 +31,72 @@ export default class ClusterRegisterStore {
   @observable
   private _state: 'ready' | 'processing' = 'ready'
 
+  @observable
+  private _registerMap = new PayloadMap<Item, Payload>(keySelector)
+
   get state() {
     return this._state
   }
 
-  @observable
-  private _length = 0
-
+  @computed
   get length() {
-    return this._length
+    return this._registerMap.size
   }
 
-  @observable
-  private _processedCount = 0
-
+  @computed
   get processedCount() {
-    return this._processedCount
-  }
-
-  @observable
-  private _currentItem: Item | null = null
-
-  get currentItem() {
-    return this._currentItem
+    return [...this._registerMap.values()].filter(({ payload }) => payload?.resolved)
   }
 
   readonly errorEvent = new EventStore<Error>()
 
-  constructor() {
+  constructor(private readonly clusterRepository: ClusterRepository) {
     makeObservable(this)
   }
 
   request = flow(function* (this: ClusterRegisterStore, items: Item[]) {
     this.logger.info(`requesting cluster register ${items.length} items`)
-    this._length = items.length
-    this._processedCount = 0
 
     this._state = 'processing'
 
-    for (const item of items) {
-      this._currentItem = item
+    this._registerMap.clear()
+    this._registerMap.update(items)
 
-      const res: CommonRes = yield (async () => {
-        const repo = container.resolve(ClusterRepository)
-        this.logger.debug(
-          `requesting cluster registration, clusterName: ${item.clusterName}, accountId: ${item.accountId}`
-        )
-        return repo.RegisterCluster(item.clusterName, item.accountId)
-      })()
+    // TODO: parallel computation?
+    // const promises: Promise<void>[] = []
 
-      if (res.getStatus() !== ResultCode.SUCCESS) {
-        this.logger.debug('failed to register cluster. reason: ', res.getMessage())
-        const err = new Error(`registeration failed: ${item.clusterName}, error: ${res.getMessage()}`)
-        this.errorEvent.emit(err)
-      }
-
-      this._processedCount += 1
+    for (const [, itemWithPayload] of this._registerMap) {
+      yield this.requestRegister(itemWithPayload)
     }
 
-    this._currentItem = null
     this._state = 'ready'
     this.logger.info('finished cluster register request')
   })
+
+  private async requestRegister(
+    itemWithPayload: ValueWithPayload<Item, Payload>
+  ): Promise<ValueWithPayload<Item, Payload>> {
+    const { accountId, clusterName } = itemWithPayload.value
+    itemWithPayload.payload = makeAutoObservable({ resolved: false })
+
+    this.logger.info(`request cluster register, clusterName: ${clusterName}, accountId: ${accountId}`)
+
+    const response = await this.clusterRepository.RegisterCluster(clusterName, accountId)
+
+    itemWithPayload.payload.response = {
+      resultCode: response.getStatus(),
+      message: response.getMessage(),
+    }
+
+    if (itemWithPayload.payload.response.resultCode !== ResultCode.SUCCESS) {
+      const errorMessage = `failed to register cluster account: ${accountId}, clusterName: ${clusterName}, reason: ${itemWithPayload.payload.response.message}`
+
+      this.logger.error(errorMessage)
+      this.errorEvent.emit(new Error(errorMessage))
+    } else {
+      this.logger.info(`register complete. clusterName: ${clusterName}, accountId: ${accountId}`)
+    }
+
+    return itemWithPayload
+  }
 }
