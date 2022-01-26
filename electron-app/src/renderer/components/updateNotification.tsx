671409e6af73388089d7bb3ac5fdf4b8bb7c7cd5
do.kim <90447762+do-kim-pubg@users.noreply.github.com>
2022-01-26 16:51:22 +0900

    feature: update notification (#87)
    * add latest updated version notification
    
    * add link to latest release / skip version button
    
    * skip update notification when skip
    
    * close snackbar on open release url
    
    * Fix messages and size
    
    Co-authored-by: Minuk Song <musong@pubg.com>

---
+++ electron-app/src/renderer/components/updateNotification.tsx
@@ -0,0 +1,126 @@
+import { SnackbarKey, useSnackbar } from 'notistack'
+import semver from 'semver'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { Box, Button, emphasize, Theme, useTheme } from '@mui/material'
+import browserLogger from '../logger/browserLogger'
+import { version } from '../../../package.json'
+
+declare global {
+  interface Window {
+    openURL(url: string): void
+  }
+}
+
+const API_URL = 'https://api.github.com/repos/pubg/kubeconfig-updater/releases/latest'
+const LATEST_PAGE_URL = 'https://github.com/pubg/kubeconfig-updater/releases/latest'
+
+interface NotificationHistory {
+  latestVersion: string
+}
+
+const notificationHistoryLocalStorageKey = 'notification'
+
+function canNotifyUpdate(latestVersion: semver.SemVer): boolean {
+  const raw = localStorage.getItem(notificationHistoryLocalStorageKey)
+  if (!raw) {
+    return true
+  }
+
+  try {
+    const data: NotificationHistory = JSON.parse(raw)
+
+    return data.latestVersion !== latestVersion.format()
+  } catch (err) {
+    browserLogger.error(err)
+    return false
+  }
+}
+
+function saveCurrentVersionNotificated(latestVersion: semver.SemVer) {
+  const data: NotificationHistory = { latestVersion: latestVersion.format() }
+  const raw = JSON.stringify(data)
+
+  localStorage.setItem(notificationHistoryLocalStorageKey, raw)
+}
+
+async function getLatestVersion(): Promise<semver.SemVer | null> {
+  try {
+    const { data } = await axios.get(API_URL)
+
+    let parsedSemver = semver.parse(data.tag_name, true)
+    if (parsedSemver === null) {
+      parsedSemver = semver.parse(data.name)
+    }
+
+    return parsedSemver
+  } catch (err) {
+    browserLogger.error(err)
+    return null
+  }
+}
+
+export default function UpdateNotification() {
+  const snackbar = useSnackbar()
+
+  useEffect(() => {
+    ;(async () => {
+      const latestVersion = await getLatestVersion()
+      if (!latestVersion) {
+        browserLogger.warn('cannot get latest version from github')
+        return
+      }
+
+      browserLogger.info(`latest version: ${latestVersion.format()}`)
+
+      const currentVersion = semver.parse(version, true)
+      if (!currentVersion) {
+        throw new Error(`current version must be parsed as semver, value: ${semver}`)
+      }
+
+      browserLogger.info(`current version: ${currentVersion.format()}`)
+
+      if (!canNotifyUpdate(latestVersion)) {
+        browserLogger.debug(`skip notifying update (it's not first run), version: ${latestVersion.format()}`)
+        return
+      }
+
+      let updateNotiSnackbarKey: SnackbarKey | null = null
+
+      const onOpenReleaseWebpage = () => {
+        if (updateNotiSnackbarKey) {
+          snackbar.closeSnackbar(updateNotiSnackbarKey)
+        }
+
+        window.openURL(LATEST_PAGE_URL)
+      }
+
+      const onSkip = () => {
+        if (updateNotiSnackbarKey) {
+          snackbar.closeSnackbar(updateNotiSnackbarKey)
+        }
+
+        saveCurrentVersionNotificated(latestVersion)
+      }
+
+      if (latestVersion.compare(currentVersion) > 0) {
+        updateNotiSnackbarKey = snackbar.enqueueSnackbar(`New Version ${latestVersion.format()} Released`, {
+          variant: 'info',
+          action: (
+            <Box display="flex" gap="1em">
+              <Button variant="text" size="small" sx={{ color: 'white' }} onClick={onOpenReleaseWebpage}>
+                OPEN PAGE
+              </Button>
+              <Button variant="text" size="small" sx={{ color: 'white' }} onClick={onSkip}>
+                DISMISS
+              </Button>
+            </Box>
+          ),
+          persist: true,
+        })
+      }
+    })()
+  }, [snackbar])
+
+  return <></>
+}
