30302a760202b3a57bb4399c0866e515f363e0d9
do.kim <do.kim@pubg.com>
2022-02-18 11:33:08 +0900

    add log rotating on startup for mainLogger

--- electron-app/src/main/logger/mainLogger.ts
+++ electron-app/src/main/logger/mainLogger.ts
@@ -1,4 +1,5 @@
 import electronLogger from 'electron-log'
+import * as fs from 'fs'
 
 // const isDevelopment = process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true'
 
@@ -7,10 +8,16 @@ const mainLogger = electronLogger.create('main')
 
 mainLogger.transports.console.useStyles = true
 mainLogger.transports.console.format = '[{y}-{m}-{d} {h}:{i}:{s}.{ms}] [{level}]{scope} {text}'
+mainLogger.transports.file.maxSize = 1048576 * 128 // 128 mb
 
 if (process.env.DEBUG_PROD !== 'true' && process.env.NODE_ENV === 'production') {
   mainLogger.transports.console.level = 'info'
   mainLogger.transports.file.level = 'info'
 }
+
+// move past logs to main.old.log and clear the current log file.
+const mainLogFilePath = mainLogger.transports.file.getFile().path
+mainLogger.transports.file.archiveLog(mainLogFilePath)
+mainLogger.transports.file.getFile().clear()
 
 export default mainLogger
