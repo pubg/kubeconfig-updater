cfc3ff4f2eed5c24c7f2385fd500259cf8f47cd7
do.kim <do.kim@pubg.com>
2022-02-22 11:58:43 +0900

    implement GetConfig, SetConfig on applicationController

--- backend/controller/application_controller/controller.go
+++ backend/controller/application_controller/controller.go
@@ -2,17 +2,20 @@ package application_controller
 
 import (
 	"context"
-
 	"github.com/pubg/kubeconfig-updater/backend/controller/protos"
+	"github.com/pubg/kubeconfig-updater/backend/pkg/service/raw_config_service"
 	"github.com/pubg/kubeconfig-updater/backend/pkg/types"
 )
 
 type applicationService struct {
+	RawConfigService *raw_config_service.Service
 	protos.UnimplementedApplicationServer
 }
 
-func NewController() protos.ApplicationServer {
-	return &applicationService{}
+func NewController(service *raw_config_service.Service) protos.ApplicationServer {
+	return &applicationService{
+		RawConfigService: service,
+	}
 }
 
 func (s *applicationService) Ping(context.Context, *protos.CommonReq) (*protos.CommonRes, error) {
@@ -24,5 +27,46 @@ func (s *applicationService) Ping(context.Context, *protos.CommonReq) (*protos.C
 func (s *applicationService) Version(context.Context, *protos.CommonReq) (*protos.CommonRes, error) {
 	return &protos.CommonRes{
 		Message: types.VERSION,
+	}, nil
+}
+
+func (s *applicationService) GetConfig(_ context.Context, req *protos.GetConfigReq) (*protos.GetConfigRes, error) {
+	cfg, err := s.RawConfigService.GetConfig(req.Name)
+	if err != nil {
+		return &protos.GetConfigRes{
+			CommonRes: &protos.CommonRes{
+				Status:  protos.ResultCode_SERVER_INTERNAL,
+				Message: err.Error(),
+			},
+			Data: "",
+		}, nil
+	}
+
+	return &protos.GetConfigRes{
+		CommonRes: &protos.CommonRes{
+			Status:  protos.ResultCode_SUCCESS,
+			Message: "Success",
+		},
+		Data: *cfg,
+	}, nil
+}
+
+func (s *applicationService) SetConfig(_ context.Context, req *protos.SetConfigReq) (*protos.SetConfigRes, error) {
+	err := s.RawConfigService.SetConfig(req.Name, req.Data)
+
+	if err != nil {
+		return &protos.SetConfigRes{
+			CommonRes: &protos.CommonRes{
+				Status:  protos.ResultCode_SERVER_INTERNAL,
+				Message: err.Error(),
+			},
+		}, nil
+	}
+
+	return &protos.SetConfigRes{
+		CommonRes: &protos.CommonRes{
+			Status:  protos.ResultCode_SUCCESS,
+			Message: "Success",
+		},
 	}, nil
 }
