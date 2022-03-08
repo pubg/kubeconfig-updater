0f2d8f1f5b92a03306fc00531670210eeeda0ef1
do.kim <do.kim@pubg.com>
2022-03-08 14:33:54 +0900

    implement electronRepository

--- electron-app/src/renderer/repositories/UI/electronRepository.ts
+++ electron-app/src/renderer/repositories/UI/electronRepository.ts
@@ -4,10 +4,10 @@ import { Repository } from './repository'
 @tsyringe.singleton()
 export class ElectronRepository implements Repository {
   get<T>(key: string): T | undefined {
-    throw new Error('Method not implemented.')
+    return clientConfigStore.get(key, undefined) as T | undefined
   }
 
   set<T>(key: string, value: T): void {
-    throw new Error('Method not implemented.')
+    clientConfigStore.set(key, value)
   }
 }
--- electron-app/src/renderer/repositories/UI/repository.ts
+++ electron-app/src/renderer/repositories/UI/repository.ts
@@ -2,6 +2,6 @@
  * @classdesc Repository is the backend for storing UI related configurations.
  */
 export interface Repository {
-  get<T>(key: string): T | undefined
-  set<T>(key: string, value: T): void
+  get<T>(key: string): NonNullable<T> | undefined
+  set<T>(key: string, value: NonNullable<T>): void
 }
