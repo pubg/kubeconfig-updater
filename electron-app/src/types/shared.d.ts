b688bdb824ba320bccd441cd5de6a28ff4909e68
do.kim <do.kim@pubg.com>
2022-03-11 17:38:43 +0900

    delete unused comments

--- electron-app/src/types/shared.d.ts
+++ electron-app/src/types/shared.d.ts
@@ -3,35 +3,6 @@ import Store from 'electron-store'
 import EventEmitter from 'events'
 
 declare global {
-  /*
-  namespace IPCType {
-    type IPCPortRequestType = 'getGrpcWebPort'
-  }
-
-  type OpenDirType = 'openLogDir' | 'openBackendConfigDir'
-
-  type IPCType =
-    | IPCType.IPCPortRequestType
-    | 'ipc-test'
-    | 'openURL'
-    | OpenDirType
-    | 'clientConfigStore'
-    | 'theme.shouldUseDarkColors'
-    | 'nativeTheme.updated'
-
-  type ExposeApiKey = 'shouldUseDarkColors' | 'nativeThemeEvent' | 'clientConfigStore' | OpenDirType
-
-  namespace Electron {
-    interface ContextBridge {
-      exposeInMainWorld(apiKey: ExposeApiKey, api: any): void
-    }
-
-    interface IpcMain {
-      on(channel: IPCType, listener: (event: IpcMainEvent, ...args: any[]) => void): this
-    }
-  }
-  */
-
   function openLogDir(): void
   function openBackendConfigDir(): void
   function shouldUseDarkColors(): boolean
